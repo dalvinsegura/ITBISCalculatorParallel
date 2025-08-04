e2168c90e8194cb9ddffafc446e0078c781b5951
Edgard <edgardmendezperez@gmail.com>
2025-08-04 14:30:06 -0400

    Desarrollo de clase venta y generarventas

--- ITBISCalculatorParallel/Program.cs
+++ ITBISCalculatorParallel/Program.cs
@@ -1,3 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
 
-Console.WriteLine("Hello, World!");
+using ITBISCalculatorParallel.Models;
+using ITBISCalculatorParallel.Services;
+
+var clase = GeneradorDeVentas.GenerarVentas(10);
+
+
+foreach (var venta in clase.GetRange(0, 3))
+{
+    Console.WriteLine(venta);
+}
