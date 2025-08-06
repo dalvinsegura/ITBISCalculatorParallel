f89a6ce815d9b1dc8ad135af32d2ce2bd4db137e
estarlin-gg <estarlingerman17@gmail.com>
2025-08-05 22:01:32 -0400

    Desarrollo Proceso Secuencial

--- ITBISCalculatorParallel/Services/ProcesadorSecuencial.cs
+++ ITBISCalculatorParallel/Services/ProcesadorSecuencial.cs
@@ -1,7 +1,30 @@
+using ITBISCalculatorParallel.Models;
+using System.Diagnostics;
+
 namespace ITBISCalculatorParallel.Services;
 
 public class ProcesadorSecuencial
 {
-    // Persona 3
-    
+    private const decimal TASA_ITBIS = 0.18m;
+
+    public void Ejecutar(List<Venta> ventas)
+    {
+        decimal totalVentas = 0;
+        decimal totalITBIS = 0;
+
+        var sw = Stopwatch.StartNew();
+
+        foreach (var venta in ventas)
+        {
+            totalVentas += venta.Monto;
+            totalITBIS += venta.Monto * TASA_ITBIS;
+        }
+
+        sw.Stop();
+
+        Console.WriteLine($"[Secuencial] Total Ventas: {totalVentas:C}");
+        Console.WriteLine($"[Secuencial] Total ITBIS: {totalITBIS:C}");
+        Console.WriteLine($"[Secuencial] Tiempo: {sw.ElapsedMilliseconds} ms");
+    }
+
 }
